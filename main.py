68dd186e1be05f9881f815ce28c8e03b3ec45a73
Thang Tran <drakiez92@gmail.com>
2022-01-20 09:13:27 +0700

    Refactor

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-from flask import Flask, send_file
-
-app = Flask(__name__,
-            static_url_path='',
-            static_folder='public')
-
-@app.route('/')
-def index():
-    return send_file('public/index.html')
+from app import app
 
 app.run('0.0.0.0', 8080)
